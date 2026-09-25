bd08b0e7ed0c8194c310cfe557b041d871b269f1
updatebotbot <266333983+updatebotbot@users.noreply.github.com>
2026-09-25 17:29:52 +0000

    Updates

--- fetch.py
+++ fetch.py
@@ -419,7 +419,6 @@ for index, appid in enumerate(queue):
         data = json_obj[str(appid)]["data"]
     except KeyError:
         progress(f"E: {appid}", end="")
-        print(text)
         state[appid] = timestamp
         error_count = error_count + 1
         sleep(args.sleep)
